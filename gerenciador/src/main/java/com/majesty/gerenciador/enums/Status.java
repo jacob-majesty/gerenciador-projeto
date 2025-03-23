705c67725cd78d9248e1f18627c58df0155d3993
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-23 16:10:03 -0300

    Adicionando enum status

---
+++ gerenciador/src/main/java/com/majesty/gerenciador/enums/Status.java
@@ -0,0 +1,12 @@
+package com.majesty.gerenciador.enums;
+
+public enum Status {
+    EM_ANALISE,
+    ANALISE_REALIZADA,
+    ANALISE_APROVADA,
+    INICIADO,
+    PLANEJADO,
+    EM_ANDAMENTO,
+    ENCERRADO,
+    CANCELADO
+}
