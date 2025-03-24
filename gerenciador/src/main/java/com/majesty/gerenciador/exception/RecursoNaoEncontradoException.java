297b65a9b40cc13d6c3af858a3e2e65913d4b0a2
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-23 21:23:22 -0300

    Adicionando classes de exception

---
+++ gerenciador/src/main/java/com/majesty/gerenciador/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.majesty.gerenciador.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
---
+++ gerenciador/src/main/java/com/majesty/gerenciador/exception/RequisicaoInvalidaException.java
@@ -0,0 +1,7 @@
+package com.majesty.gerenciador.exception;
+
+public class RequisicaoInvalidaException extends RuntimeException {
+    public RequisicaoInvalidaException(String message) {
+        super(message);
+    }
+}
