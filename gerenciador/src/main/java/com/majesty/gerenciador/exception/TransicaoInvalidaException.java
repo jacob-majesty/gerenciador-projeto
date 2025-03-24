d3daa8a4c9f81e4246bef43273a8a47230b41454
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-23 21:24:20 -0300

    Adicionando TransicaoInvalidaException

---
+++ gerenciador/src/main/java/com/majesty/gerenciador/exception/TransicaoInvalidaException.java
@@ -0,0 +1,7 @@
+package com.majesty.gerenciador.exception;
+
+public class TransicaoInvalidaException extends RuntimeException{
+    public TransicaoInvalidaException(String message) {
+        super(message);
+    }
+}
