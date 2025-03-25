aa273d1fbfb4d9a3b2f4bd3887bade8bc7d0eb65
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-25 00:08:58 -0300

    Adicionando uma query no Membro Repository

--- gerenciador/src/main/java/com/majesty/gerenciador/repository/MembroRepository.java
+++ gerenciador/src/main/java/com/majesty/gerenciador/repository/MembroRepository.java
@@ -2,7 +2,13 @@ package com.majesty.gerenciador.repository;
 
 import com.majesty.gerenciador.entity.Membro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MembroRepository extends JpaRepository<Membro, Long> {
-
+    @Query("SELECT COUNT(p) FROM Membro m " +
+            "JOIN m.projetosAlocados p " +
+            "WHERE m.id = :membroId " +
+            "AND p.statusAtual NOT IN ('ENCERRADO', 'CANCELADO')")
+    long contarProjetosAtivosPorMembro(@Param("membroId") Long membroId);
 }
