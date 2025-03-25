80db7ee40fc417bfa6ecf86ccd59566f8859a86e
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-25 00:38:26 -0300

    fix: deletando transicao exception

--- gerenciador/src/main/java/com/majesty/gerenciador/exception/TransicaoInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package com.majesty.gerenciador.exception;
-
-public class TransicaoInvalidaException extends RuntimeException{
-    public TransicaoInvalidaException(String message) {
-        super(message);
-    }
-}
